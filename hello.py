6eca740d6b51a4d7eb52d3c0e0d6313c1d7a8c21
jamiemsanson <jmsanson@hotmail.com>
2020-11-25 14:39:03 +1300

    Added the code and changed

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("Kia ora koutou,")
+print("")
+print("This is a simple python program.")
+print("What does it do? Just prints out this message.")
+print("")
+print("Ngā mihi, Evan & Rebecca")
+print("test")
